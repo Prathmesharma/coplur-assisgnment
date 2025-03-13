d6457932bbde7fd5ac150506a73a6ba0ea14c988
Prathmesharma <152026744+Prathmesharma@users.noreply.github.com>
2025-03-13 14:38:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,140 @@
+﻿
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Customer cust = new Customer();
+        cust.CustomerId = 52;
+        cust.FirstName = "Prathmesh";
+        cust.LastName = "Sharma";
+        cust.Email = "prathmesh2004sharma@gmail.com";
+        cust.Type = CustomerType.Silver;
+
+        cust.ProcessOrder(cust);
+        cust.Register(cust);
+
+        Console.WriteLine(cust.GetDiscountPercentage() + " percent");
+        Leads lead = new Leads();
+        lead.Name = "Gappu";
+        lead.Email = "gappu@gmail.com";
+        lead.Check();
+
+
+
+    }
+}
+
+
+public class Customer
+{
+    public int CustomerId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public CustomerType Type { get; set; }
+    public void Register(Customer cust)
+    {
+        try
+        {
+            Console.WriteLine(cust.FirstName + " " + cust.LastName + "  ->" + cust.Type + " customer");
+            
+            if (cust.Type == CustomerType.Bronze)
+            {
+                //some logic
+                Console.WriteLine("Checking Bronze customer details");
+            }
+
+            if (cust.Type == CustomerType.Silver)
+            {
+                //some logic
+                Console.WriteLine("Checking Silver customer details");  
+            }
+
+            if (cust.Type == CustomerType.Gold)
+            {
+                //some logic
+                Console.WriteLine("Checking Gold customer details");
+            }
+
+            if (cust.Type == CustomerType.Platinum)
+            {
+                //some logic
+                Console.WriteLine("Checking Platinum customer details");
+            }
+
+            SaveCustomer();
+
+        }
+        catch (Exception ex)
+        {
+            //Logger 
+            Console.WriteLine($"Log Error: {ex.Message}");
+        }
+    }
+
+    public virtual bool SaveCustomer()
+    {
+        //save customer details to database
+        Console.WriteLine("Details Saved ");
+        return true;
+    }
+
+    public int GetDiscountPercentage()
+    {
+        if (this.Type == CustomerType.Bronze)
+        {
+            return 15;
+        }
+
+        if (this.Type == CustomerType.Silver)
+        {
+            return 20;
+        }
+
+        if (this.Type == CustomerType.Gold)
+        {
+            return 25;
+        }
+
+        if (this.Type == CustomerType.Platinum)
+        {
+            return 27;
+        }
+
+        return 12;
+    }
+
+    public void ProcessOrder(Customer cust)
+    {
+        GetDiscountPercentage();
+        //Process Payment after Discount
+        Console.WriteLine("Processing for->" + cust.FirstName + " " + cust.LastName);
+        //Send confirmation
+        int dis=cust.GetDiscountPercentage();
+
+        //Log Order Activity
+        Console.WriteLine(dis + " percent discount ");
+        Console.WriteLine("Order Placed Successfully");
+    }
+}
+
+public class Leads : Customer
+{
+    //It is just a lead so need to save it in the database
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public void Check()
+    {
+        Console.WriteLine(Name+" Checked details");
+    }
+}
+
+public enum CustomerType
+{
+    Bronze,
+    Silver,
+    Gold,
+    Platinum
+}
